e0b9872ac5200b18720bdf9c86e0ef28d8b1308d
matthewlai <mklai10@students.cs.ubc.ca>
2024-03-24 14:06:04 -0700

    database

--- out/production/project_n8y4b_v2x6g_v5l3b/README.md
+++
@@ -1,5 +0,0 @@
-# project_n8y4b_v2x6g_v5l3b
-
-# Project Description
-
-We aim to create a database application that displays match information for League Of Legends, an online video game where two teams of players fight to destroy the other team's home base. Users may query information about the live state of an online match such as player strength statistics and the progress of destroyed structures.
---
+++ src/database/DatabaseConnectionHandler.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionHandler {
+    private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
+    private static final String EXCEPTION_TAG = "[EXCEPTION]";
+    private static final String WARNING_TAG = "[WARNING]";
+
+    private Connection connection = null;
+
+    public DatabaseConnectionHandler() {
+        try {
+            // Load the Oracle JDBC driver
+            // Note that the path could change for new drivers
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+}
